40697fabedc55827eba3b50655af8f6be5395689
Victor Manuel Soesilo <vmanuels0083@gmail.com>
2024-01-08 00:46:39 +0700

    feat: update alvin rony done

--- resources/views/admin/nota/edit.blade.php
+++ resources/views/admin/nota/edit.blade.php
@@ -8,17 +8,19 @@
             @method('put')
             <div class="form-group">
                 <label for="total_pembayaran">Total Pembayaran</label>
-                <input type="number" step="0.01" class="form-control" id="total_pembayaran" name="total_pembayaran" value="{{ $nota->total_pembayaran }}" required>
+                <input type="number" step="0.01" class="form-control" id="total_pembayaran" name="total_pembayaran"
+                    value="{{ $nota->total_pembayaran }}" required>
             </div>
             <div class="form-group">
-                <label for="status_pengiriman">Status Pengiriman</label>
-                <select name="status_pengiriman" class="form-control">
-                    @foreach (['Menunggu Pembayaran', 'Diproses', 'Dikirim', 'Diterima'] as $status)
-                        <option value="{{ $status }}" {{ $nota->status_pengiriman == $status ? 'selected' : '' }}>
-                            {{ $status }}
-                        </option>
-                    @endforeach
-                </select>
+                <label for="total_diskon">Total Diskon</label>
+                <input type="number" class="form-control" id="total_diskon" name="total_diskon"
+                    value="{{ $nota->total_diskon }}" required>
+            </div>
+            <div class="form-group">
+                <label for="total_pembayaran_diskon">Total Pembayaran Diskon</label>
+                <input type="number" class="form-control" id="total_pembayaran_diskon" name="total_pembayaran_diskon"
+                    value="{{ $nota->total_pembayaran_diskon }}" required>
+                required>
             </div>
             <div class="form-group">
                 <label for="user_id">User</label>
@@ -34,7 +36,8 @@
                 <label for="metode_pembayaran_id">Metode Pembayaran</label>
                 <select name="metode_pembayaran_id" class="form-control">
                     @foreach ($metodePembayarans as $metodePembayaran)
-                        <option value="{{ $metodePembayaran->id }}" {{ $nota->metodePembayaran->id == $metodePembayaran->id ? 'selected' : '' }}>
+                        <option value="{{ $metodePembayaran->id }}"
+                            {{ $nota->metodePembayaran->id == $metodePembayaran->id ? 'selected' : '' }}>
                             {{ $metodePembayaran->nama }}
                         </option>
                     @endforeach
@@ -44,7 +47,8 @@
                 <label for="alamat_pengiriman_id">Alamat Pengiriman</label>
                 <select name="alamat_pengiriman_id" class="form-control">
                     @foreach ($alamatPengirimans as $alamatPengiriman)
-                        <option value="{{ $alamatPengiriman->id }}" {{ $nota->alamatPengiriman->id == $alamatPengiriman->id ? 'selected' : '' }}>
+                        <option value="{{ $alamatPengiriman->id }}"
+                            {{ $nota->alamatPengiriman->id == $alamatPengiriman->id ? 'selected' : '' }}>
                             {{ $alamatPengiriman->nama_penerima }}
                         </option>
                     @endforeach
@@ -54,8 +58,26 @@
                 <label for="jenis_pengiriman_id">Jenis Pengiriman</label>
                 <select name="jenis_pengiriman_id" class="form-control">
                     @foreach ($jenisPengirimans as $jenisPengiriman)
-                        <option value="{{ $jenisPengiriman->id }}" {{ $nota->jenisPengiriman->id == $jenisPengiriman->id ? 'selected' : '' }}>
+                        <option value="{{ $jenisPengiriman->id }}"
+                            {{ $nota->jenisPengiriman->id == $jenisPengiriman->id ? 'selected' : '' }}>
                             {{ $jenisPengiriman->nama }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="status_pembayaran">Status Pembayaran</label>
+                <select class="form-control" id="status_pembayaran" name="status_pembayaran">
+                    <option value="Belum Dibayar" @if ($nota->statusPembayaran == 'Belum Dibayar' ? 'selected' : '')  @endif>Belum Dibayar</option>
+                    <option value="Lunas" @if ($nota->statusPembayaran == 'Lunas' ? 'selected' : '')  @endif>Lunas</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="status_pengiriman">Status Pengiriman</label>
+                <select name="status_pengiriman" class="form-control">
+                    @foreach (['Menunggu Pembayaran', 'Persiapan Barang', 'Siap Diantar', 'Pengiriman', 'Pesanan Diterima', 'Pesanan Selesai'] as $status)
+                        <option value="{{ $status }}" {{ $nota->status_pengiriman == $status ? 'selected' : '' }}>
+                            {{ $status }}
                         </option>
                     @endforeach
                 </select>
@@ -63,5 +85,5 @@
             <button type="submit" class="btn btn-primary">Update Nota</button>
         </form>
     </div>
-</div>
+    </div>
 @endsection
