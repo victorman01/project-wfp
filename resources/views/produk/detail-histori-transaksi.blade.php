6e87065db1ffdbde1655fca456b56c53c982cd1f
ToastedBaguette <ronyhartonoirawan@gmail.com>
2024-01-08 01:03:15 +0700

    Fix: Update UI Histori Transaksi & Detail Transaksi

--- resources/views/produk/detail-histori-transaksi.blade.php
+++ resources/views/produk/detail-histori-transaksi.blade.php
@@ -5,12 +5,82 @@
 @endsection
 
 @section('content')
-    <div class="container mt-3">
-        <a href="{{ route('historiTransaksi') }}" class="btn btn-outline-primary">Kembali</a>
-    </div>
-    <div class="container my-5 mb-10">
+    <div class="container ">
+        {{-- CARD DAFTAR TRANSAKSI --}}
+        <div class="card my-10 mb-20">
+            <div class="card-header ">
+                <h2 class="text-center text-primary">Detail Transaksi</h2>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        {{-- CARD INVOICE --}}
+                        <div class="card my-1 h-100">
+                            <div class="card-header">
+                                <h5 class="card-title">Status: <span class="badge bg-success rounded">Selesai</span>
+                                </h5>
+                            </div>
+                            <div class="card-body p-2 px-8">
+                                <div class="row my-5">
+                                    <p class="text-muted">No. Invoice: <span class="text-dark fw-bold">1</span></p>
+                                    <p class="text-muted">Tanggal Pembelian: <span class="text-dark fw-bold">08 Jan
+                                            2024</span></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-    </div>
+                    <div class="col">
+                        {{-- CARD ALAMAT --}}
+                        <div class="card my-1">
+                            <div class="card-header">
+                                <h5 class="card-title">Info Pengiriman:
+                                </h5>
+                            </div>
+                            <div class="card-body p-2 px-8">
+                                <div class="row my-5">
+                                    <p class="text-muted">Kurir: <span class="text-dark">JNE-Sameday</span></p>
+                                    <p class="text-muted">Alamat:</p>
+                                    <p>Nama Jalan <br> Kota, Provinsi <br> Nomor HP</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-    <div class="mb-10"></div>
+
+
+                <div class="card my-3">
+                    <div class="card-header">
+                        <h5 class="card-title">Belanja, 05 Jan 2024 <span class="badge bg-success rounded">Terkirim</span>
+                        </h5>
+                    </div>
+                    <div class="card-body p-2 px-8">
+                        <div class="row my-5" id="histori_transaksi">
+                            <div class="col-2 text-end">
+                                <img src="{{ asset('sandbox360\img\photos\cf3.jpg') }}" alt="Product Image"
+                                    class=" img rounded shadow" style="width:140px;height:140px;object-fit: cover;">
+                            </div>
+
+                            <div class="col-4">
+                                <label for="">Produk A</label>
+
+                                <h3 class="post-title h3">Jenis: A++</h3>
+                                <p class="text-muted">1 Barang x Rp59000,-</p>
+                                <p id="harga_total">+1 produk Lainnya</p>
+                            </div>
+                            <div class="col-3 offset-3">
+                                <br>
+                                <p class="text-muted m-0">Total Belanja</p>
+                                <h3 class="post-title h3 m-0 mb-3">Rp120000,-</h3>
+                            </div>
+                            <div class="text-end">
+                                <a href="" class="btn btn-primary">Lihat Detail Transaksi</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/produk/histori-transaksi.blade.php
+++ resources/views/produk/histori-transaksi.blade.php
@@ -12,43 +12,37 @@
                 <h2 class="text-center text-primary">Detail Transaksi</h2>
             </div>
             <div class="card-body">
-                <div class="row">
-                    <div class="col">
-                        {{-- CARD INVOICE --}}
-                        <div class="card my-1">
-                            <div class="card-header">
-                                <h5 class="card-title">Status: <span class="badge bg-success rounded">Selesai</span>
-                                </h5>
-                            </div>
-                            <div class="card-body p-2 px-8">
-                                <div class="row my-5">
-                                    <p class="text-muted">No. Invoice: <span class="text-dark fw-bold">1</span></p>
-                                    <p class="text-muted">Tanggal Pembelian: <span class="text-dark fw-bold">08 Jan
-                                            2024</span></p>
-                                </div>
-                            </div>
-                        </div>
+                {{-- CARD TRANSAKSI --}}
+                <div class="card my-3">
+                    <div class="card-header">
+                        <h5 class="card-title">Belanja, 05 Jan 2024 <span class="badge bg-success rounded">Terkirim</span>
+                        </h5>
                     </div>
-
-                    <div class="col">
-                        {{-- CARD ALAMAT --}}
-                        <div class="card my-1">
-                            <div class="card-header">
-                                <h5 class="card-title">Info Pengiriman:
-                                </h5>
+                    <div class="card-body p-2 px-8">
+                        <div class="row my-5" id="histori_transaksi">
+                            <div class="col-2 text-end">
+                                <img src="{{ asset('sandbox360\img\photos\cf3.jpg') }}" alt="Product Image"
+                                    class=" img rounded shadow" style="width:140px;height:140px;object-fit: cover;">
                             </div>
-                            <div class="card-body p-2 px-8">
-                                <div class="row my-5">
-                                    <p class="text-muted">Kurir: <span class="text-dark">JNE-Sameday</span></p>
-                                    <p class="text-muted">Alamat:</p>
-                                    <p>Nama Jalan <br> Kota, Provinsi <br> Nomor HP</p>
-                                </div>
+
+                            <div class="col-4">
+                                <label for="">Produk A</label>
+
+                                <h3 class="post-title h3">Jenis: A++</h3>
+                                <p class="text-muted">1 Barang x Rp59000,-</p>
+                                <p id="harga_total">+1 produk Lainnya</p>
+                            </div>
+                            <div class="col-3 offset-3">
+                                <br>
+                                <p class="text-muted m-0">Total Belanja</p>
+                                <h3 class="post-title h3 m-0 mb-3">Rp120000,-</h3>
+                            </div>
+                            <div class="text-end">
+                                <a href="" class="btn btn-primary">Lihat Detail Transaksi</a>
                             </div>
                         </div>
                     </div>
                 </div>
-
-
 
                 <div class="card my-3">
                     <div class="card-header">
