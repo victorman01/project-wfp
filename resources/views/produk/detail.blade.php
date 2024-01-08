51c3f423ec2c39bd1c2c356a8f224c4dd4701b38
alvinfersus <alvinfersus@gmail.com>
2024-01-09 00:11:06 +0700

    Fix: display discount & fix input max by stok

--- resources/views/produk/detail.blade.php
+++ resources/views/produk/detail.blade.php
@@ -11,7 +11,8 @@
                 {{-- @foreach ($produk->gambar as $gambar)
                     <img src="{{ asset('storage/' . $gambar->path) }}" alt="">
                 @endforeach --}}
-                <img src="{{ asset('storage/' . $produk->gambar[0]->path) }}" alt="Product Image" class="img-fluid rounded-4" /></a>
+                <img src="{{ asset('storage/' . $produk->gambar[0]->path) }}" alt="Product Image"
+                    class="img-fluid rounded-4" /></a>
             </div>
             <div class="col-md-8 product-container">
                 <h1>{{ $produk->nama }}</h1>
@@ -31,7 +32,7 @@
 
                         <div class="row">
                             @foreach ($produk->jenisProduk as $jp)
-                                @if ($jp == $produk->jenisProduk->first())
+                                {{-- @if ($jp == $produk->jenisProduk->first())
                                     <button id="{{ $jp->id }}" class="col-2 m-2 btn btn-primary"
                                         onclick="JenisProdukChange(this.id, '{{ $jp->spesifikasi }}', '{{ number_format($jp->harga, 0, ',', '.') }}', '{{ $jp->stok }}', '{{ $jp->id }}')">{{ $jp->nama }}
                                     </button>
@@ -39,7 +40,21 @@
                                     <button id="{{ $jp->id }}" class="col-2 m-2 btn btn-outline-primary"
                                         onclick="JenisProdukChange(this.id, '{{ $jp->spesifikasi }}', '{{ number_format($jp->harga, 0, ',', '.') }}', '{{ $jp->stok }}', '{{ $jp->id }}')">{{ $jp->nama }}
                                     </button>
-                                @endif
+                                @endif --}}
+
+                                @php
+                                    $checkDiskon = $jp
+                                        ->diskonProduk()
+                                        ->where('jenis_produk_id', $jp->id)
+                                        ->where('periode_mulai', '<=', now())
+                                        ->where('periode_berakhir', '>=', now())
+                                        ->first();
+                                @endphp
+
+                                <button id="{{ $jp->id }}"
+                                    class="col-2 m-2 btn {{ $jp == $produk->jenisProduk->first() ? 'btn-primary' : 'btn-outline-primary' }}"
+                                    onclick="JenisProdukChanges(this.id, '{{ json_encode($jp) }}', '{{ isset($checkDiskon) ? $produk->jenisProduk[0]->diskonProduk[0]->diskon : 0}}')">{{ $jp->nama }}
+                                </button>
                             @endforeach
                         </div>
                     </div>
@@ -47,9 +62,21 @@
 
                 <div class="card my-2 border-dark">
                     <div class="card-body">
-                        <p><b>Harga: Rp</b><span
-                                id="harga">{{ number_format($produk->jenisProduk[0]->harga, 0, ',', '.') }}</span>
-                        </p>
+                        @php
+                            $checkDiskon = $produk->jenisProduk[0]
+                                ->diskonProduk()
+                                ->where('jenis_produk_id', $produk->jenisProduk[0]->id)
+                                ->where('periode_mulai', '<=', now())
+                                ->where('periode_berakhir', '>=', now())
+                                ->first();
+
+                            if (isset($checkDiskon)) {
+                                $hargaSetelahDisc = ($produk->jenisProduk[0]->harga * (100 - $produk->jenisProduk[0]->diskonProduk[0]->diskon)) / 100;
+                                echo '<p id="tampilan"><b>Harga: Rp</b><span id="harga" style="text-decoration: line-through; color:red;">' . number_format($produk->jenisProduk[0]->harga, 0, ',', '.') . '</span><span id="harga_diskon"> ' . number_format($hargaSetelahDisc, 0, ',', '.') . '</span></p>';
+                            } else {
+                                echo '<p id="tampilan"><b>Harga: Rp</b><span id="harga">' . number_format($produk->jenisProduk[0]->harga, 0, ',', '.') . '</span></p>';
+                            }
+                        @endphp
                         <p><b>Jumlah Stok:</b> <span class="text-success" id="stok">
                                 {{ $produk->jenisProduk[0]->stok }}</span></p>
                     </div>
@@ -60,9 +87,9 @@
                         @csrf
                         <div class="col">
                             <div class="d-flex align-items-center mt-2">
-                                <label for="quantity" class="me-2">Quantity:</label>
+                                <label for="quantity" class="me-2">Jumlah:</label>
                                 <input type="number" id="quantity" class="form-control" value="1" min="1"
-                                    name="quantity">
+                                    name="quantity" max="{{ $produk->jenisProduk[0]->stok }}" oninput="checkJumlah(this.id, '{{ $produk->jenisProduk[0]->stok }}')">
                                 <input type="hidden" name="jenisProdukID" id="jenisProdukID"
                                     value="{{ $produk->jenisProduk[0]->id }}">
                             </div>
@@ -74,7 +101,7 @@
                                 onclick="Fav({{ $produk->id }})" id="btn-fav">Favorit<i class="ms-2 uil uil-heart"></i>
                             </a>
 
-                            <button type="submit" class="btn btn-primary mt-2" id="btn-submit" 
+                            <button type="submit" class="btn btn-primary mt-2" id="btn-submit"
                                 {{ $produk->jenisProduk[0]->stok == 0 ? 'disabled' : '' }}>{{ $produk->jenisProduk[0]->stok == 0 ? 'Stok Habis' : 'Tambahkan Ke Keranjang' }}</button>
                         </div>
                     </form>
@@ -101,16 +128,52 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script>
-        function JenisProdukChange(eventId, spek, hargaValue, stokValue, jenisProdukId) {
+        //{ OLD }
+        // function JenisProdukChange(eventId, spek, hargaValue, stokValue, jenisProdukId) {
+        //     var spesifikasi = document.getElementById("spesifikasi");
+        //     var harga = document.getElementById("harga");
+        //     var stok = document.getElementById("stok");
+        //     var jenisProduk = document.getElementById("jenisProdukID");
+
+        //     spesifikasi.innerHTML = spek;
+        //     harga.innerHTML = hargaValue;
+        //     stok.innerHTML = stokValue;
+        //     jenisProduk.value = jenisProdukId;
+
+        //     // Mengubah semua class btn jenis produk menjadi outlined
+        //     let sibling = document.getElementById(eventId).parentNode.firstChild;
+        //     while (sibling) {
+        //         sibling.className = "col-2 m-2 btn btn-outline-primary"
+        //         sibling = sibling.nextSibling;
+        //     }
+
+        //     // Mengubah semua class btn jenis produk terpilih menjadi primary
+        //     document.getElementById(eventId).className = "col-2 m-2 btn btn-primary";
+        //     if (stok.val() == 0) {
+        //         $('#btn-submit').prop('disabled', true)
+        //     } else {
+        //         $('#btn-submit').prop('disabled', false)
+        //     }
+        // }
+
+        function JenisProdukChanges(eventId, jenisProduk, besarDiskon) {
+            var jenisProdukJson = JSON.parse(jenisProduk);
+
             var spesifikasi = document.getElementById("spesifikasi");
-            var harga = document.getElementById("harga");
             var stok = document.getElementById("stok");
             var jenisProduk = document.getElementById("jenisProdukID");
 
-            spesifikasi.innerHTML = spek;
-            harga.innerHTML = hargaValue;
-            stok.innerHTML = stokValue;
-            jenisProduk.value = jenisProdukId;
+            spesifikasi.innerHTML = jenisProdukJson['spesifikasi'];
+            stok.innerHTML = jenisProdukJson['stok'];
+            jenisProduk.value = jenisProdukJson['id'];
+
+            var hargaAsli = jenisProdukJson['harga'].toLocaleString('id-ID');
+            var hargaSetelahDiskon = (jenisProdukJson['harga'] * (100-besarDiskon) / 100).toLocaleString('id-ID');
+            if(besarDiskon != 0){
+                $('#tampilan').html('<b>Harga: Rp</b><span id="harga" style="text-decoration: line-through; color:red;">' + hargaAsli  + '</span><span id="harga_diskon"> ' + hargaSetelahDiskon + '</span>');
+            } else {
+                $('#tampilan').html('<p id="tampilan"><b>Harga: Rp</b><span id="harga">' + hargaAsli + '</span></p>');
+            }
 
             // Mengubah semua class btn jenis produk menjadi outlined
             let sibling = document.getElementById(eventId).parentNode.firstChild;
@@ -121,10 +184,18 @@
 
             // Mengubah semua class btn jenis produk terpilih menjadi primary
             document.getElementById(eventId).className = "col-2 m-2 btn btn-primary";
-            if(stok.val() == 0){
+            if (stok.value == 0) {
                 $('#btn-submit').prop('disabled', true)
             } else {
                 $('#btn-submit').prop('disabled', false)
+            }
+        }
+
+        function checkJumlah(idInput, stok){
+            var jumlahVal = document.getElementById(idInput);
+
+            if(parseInt(jumlahVal.value) > parseInt(stok)){
+                jumlahVal.value = stok;
             }
         }
 
