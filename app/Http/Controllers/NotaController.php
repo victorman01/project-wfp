bb9951ac24169e64e39004480b454e98718e50f7
Victor Manuel Soesilo <vmanuels0083@gmail.com>
2024-01-09 00:59:48 +0700

    fix: nota bug dikit

--- app/Http/Controllers/NotaController.php
+++ app/Http/Controllers/NotaController.php
@@ -117,10 +117,10 @@ class NotaController extends Controller
             'status_pengiriman' => 'required|in:Menunggu Pembayaran,Persiapan Barang,Siap Diantar,Pengiriman,Pesanan Diterima,Pesanan Selesai',
             'status_pembayaran' => 'required|in:Lunas,Belum Dibayar',
         ]);
-        if($validatedData['status_pembayaran'] =='Lunas'){
-            $validatedData['status_pengiriman']=='Persiapan Barang';
+        if($validatedData['status_pembayaran'] ==='Lunas'){
+            $validatedData['status_pengiriman']='Persiapan Barang';
         };
-        $nota = Nota::create($validatedData);
+        $nota->update($validatedData);
         return redirect('/admin/notas')->with('success', 'Nota has been updated!');
 
     }
